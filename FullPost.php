4a83c3b055567eb6795adfbb4b0395a5e84c57cb
forid <saintblue022@gmail.com>
2021-09-17 20:50:49 +0600

    30th class

--- FullPost.php
+++ FullPost.php
@@ -38,13 +38,14 @@ if (isset($_POST["submit"])) {
 
      global $ConnectingDB;
 
-    $sql = "INSERT INTO comments(datetime,name,email,comment,approvedby,status)";
-    $sql.= "VALUES(:dateTime,:name,:email,:comment,'Pending','OFF')";
+    $sql = "INSERT INTO comments(datetime,name,email,comment,approvedby,status,post_id)";
+    $sql.= "VALUES(:dateTime,:name,:email,:comment,'Pending','OFF',:PostIdFromURL)";
     $stmt= $ConnectingDB->prepare($sql); 
     $stmt->bindValue(':dateTime',$DateTime);
     $stmt->bindValue(':name',$Name);
     $stmt->bindValue(':email',$Email);
-    $stmt->bindValue(':comment',$Comment);  
+    $stmt->bindValue(':comment',$Comment);
+    $stmt->bindValue(':PostIdFromURL',$SearchQueryParameter);  
     $Execute=$stmt->execute();
 
     if ($Execute) {
